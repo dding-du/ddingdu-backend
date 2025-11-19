058566eb13e05fd97421f5f66571786dec4a238b
Go Yeongmin <ko07073@gmail.com>
2025-11-19 19:21:48 +0900

    first commit

---
+++ src/test/java/com/ddingdu/chatbot_backend/ChatbotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddingdu.chatbot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
