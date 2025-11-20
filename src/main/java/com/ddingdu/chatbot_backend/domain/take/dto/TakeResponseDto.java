eb43fb539e1c54a44da171498829c1686e878fc5
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-11-21 05:50:05 +0900

    feat: update myLecture endpoint to include userId as a path variable and clean up TakeResponseDto

--- src/main/java/com/ddingdu/chatbot_backend/domain/take/dto/TakeResponseDto.java
+++ src/main/java/com/ddingdu/chatbot_backend/domain/take/dto/TakeResponseDto.java
@@ -2,12 +2,10 @@ package com.ddingdu.chatbot_backend.domain.take.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
-@NoArgsConstructor
 @AllArgsConstructor
 public class TakeResponseDto {
 
